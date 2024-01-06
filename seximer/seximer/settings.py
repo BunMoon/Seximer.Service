4188c944df1e2053d909c8a37e59ddf019927f73
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2024-01-06 22:52:29 +0700

    Create accounts application
    Create User Model and Register UserAdmin

--- seximer/seximer/settings.py
+++ seximer/seximer/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
